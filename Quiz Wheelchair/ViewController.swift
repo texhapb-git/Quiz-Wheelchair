894e7361d1e34b852d41ccfadff6f80f5d8a956a
TexHaPb <texhapb@bk.ru>
2021-03-06 19:55:20 +0300

    Initial Commit

---
+++ Quiz Wheelchair/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz Wheelchair
+//
+//  Created by Максим Иванов on 06.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
